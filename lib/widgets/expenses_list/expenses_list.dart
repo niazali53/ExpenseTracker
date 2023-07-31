26cb5f16fc3a0efd30936741dcefe93a93ab7b3a
NiazAli <niyazali53@gmail.com>
2023-07-31 12:37:33 +0500

    first commit

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,20 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({required this.expenses, super.key});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) {
+        return ExpenseItem(expenses[index]);
+      },
+    );
+  }
+}
